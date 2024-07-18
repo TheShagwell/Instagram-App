237ee9234e1eace250844b65b93d211319f21b88
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-18 18:37:55 +0100

    Updated...

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Tabs() {
+  return <Text>Creating my first APP with Expo</Text>;
+}
---
+++ src/app/about.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View>
+      <View>
+        <Text>Creating my first APP</Text>
+        <Text>Shagwell is online...</Text>
+        <Text>You can keep going without look back.</Text>
+      </View>
+    </View>
+  );
+}
