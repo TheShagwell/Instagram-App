ee86ad7582862b967825d55ffcc3daad50d7ce03
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-19 05:32:49 +0100

    Updated the components and worked on the installation of nativewind

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,41 @@
 import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ headerTitle: "Go Nigeria" }} />
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Go Nigeria",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={28} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="new"
+        options={{
+          headerTitle: "Create post",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus-square-o" size={28} color={color} />
+          ),
+        }}
+      />
+
+<Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={28} color={color} />
+          ),
+        }}
+      />
+
     </Tabs>
   );
 }
