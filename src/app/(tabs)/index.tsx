0cbbe398ada4618dd5348b415609be5c13ecb41e
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-19 06:16:57 +0100

    Updated tsconfig to shorten the "import" of '../../../' to '~/'

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
+import posts from '~/assets/data/posts.json';
+
+const post1 = posts[0];
 
 export default function Tabs() {
+  console.log(post1)
+
   return (
     <View className="bg-slate-600 flex-1 items-center justify-center m-10">
-      <Text className="text-3xl font-bold text-white">Creating my first APP with Expo</Text>
+      <Text className="text-3xl font-bold text-white">Join my first PODCAST.</Text>
     </View>
   );
 }
