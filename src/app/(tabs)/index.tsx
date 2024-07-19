8a64e28be17cb62890b4fece30ae1bf0f64b965a
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-19 06:00:11 +0100

    Updated by adding dummy data in json format to work with at the moment

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Tabs() {
-  return <Text>Creating my first APP with Expo</Text>;
+  return (
+    <View className="bg-slate-600 flex-1 items-center justify-center m-10">
+      <Text className="text-3xl font-bold text-white">Creating my first APP with Expo</Text>
+    </View>
+  );
 }
