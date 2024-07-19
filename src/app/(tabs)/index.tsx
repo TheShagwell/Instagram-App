db34188836c174d5bb6c2c060425fe7dc63fd879
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-19 06:57:27 +0100

    Using "props" and working with <FlatList/> to render data in list and also iterable lists in flat list format

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, FlatList, View } from "react-native";
 import posts from '~/assets/data/posts.json';
 import { Ionicons, Feather, AntDesign } from '@expo/vector-icons'
+import PostItemList from "~/src/components/PostListItem";
 
-const post1 = posts[0];
 
 export default function Tabs() {
-  console.log(post1)
-
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <PostItemList post={item}/>}
+    />  
+  )
   
+  // return (
+  //   <View>
+  //     <PostItemList post={posts[0]} />
+  //     <PostItemList post={posts[1]}/>
+  //   </View>
+  // )  
 }
