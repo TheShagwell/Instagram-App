2638c08f4e64ea120db8cc351e2183e992743b4b
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-23 02:13:46 +0100

    Updated the "AuthProvider"

--- src/providers/AuthProviders.tsx
+++ src/providers/AuthProviders.tsx
@@ -1,24 +1,47 @@
 import { Session, User } from "@supabase/supabase-js";
 import {
-    PropsWithChildren,
-    createContext,
-    useContext,
-    useState,
-    useEffect
-} from 'react'
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 import { ActivityIndicator } from "react-native";
-import { supabase } from "../lib/supabase";
+import { supabase } from "~/src/lib/supabase";
 
 type Auth = {
-    isAuthenticated: boolean;
-    session: Session | null;
-    user?: User;
-}
+  isAuthenticated: boolean;
+  session: Session | null;
+  user?: User;
+};
 
 const AuthContext = createContext<Auth>({
-    isAuthenticated: false,
-    session: null,
+  isAuthenticated: false,
+  session: null,
 });
 
+export default function AuthProvider({ children }: PropsWithChildren) {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isReady, setIsReady] = useState(false);
 
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setIsReady(true);
+    });
 
+    supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session);
+    })
+  }, []);
+
+  if(!isReady) return <ActivityIndicator/>
+
+  return (
+    <AuthContext.Provider value={{ session, user: session?.user, isAuthenticated: !!session?.user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext);
