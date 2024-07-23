68fb11537470b37bbf472d2176c359f5de3fd380
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-07-23 11:01:45 +0100

    Updated...

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -7,19 +7,24 @@ import {
   TextInput,
   Pressable,
 } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
 import Button from "~/src/components/Button";
 import { uploadImage } from "~/src/lib/cloudinary";
+import { supabase } from "~/src/lib/supabase";
+import { useAuth } from "~/src/providers/AuthProviders";
+import { router } from "expo-router";
 
 export default function Tabs() {
   const [caption, setCaption] = useState("");
-  const [image, setImage] = useState<string | null >(null);
+  const [image, setImage] = useState<string | null>(null);
+
+  const { session } = useAuth();
 
   useEffect(() => {
     if (!image) {
       pickImage();
-    } 
-  }, [image])
+    }
+  }, [image]);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -38,27 +43,38 @@ export default function Tabs() {
   };
 
   const createPost = async () => {
-    if(!image){
-      return
+    if (!image) {
+      return;
     }
     const response = await uploadImage(image);
-    console.log("image id", response?.public_id)
-  }
+    console.log("image id", response?.public_id);
+
+    const { data, error } = await supabase
+      .from('posts')
+      .insert([
+        { caption, image: response?.public_id, user_id: session?.user.id },
+      ])
+      .select();
+
+    // console.log(data);
+    // console.log(error)
+
+    router.push('/(tabs)');
+  };
 
   return (
     <View className="p-3 items-center flex-1">
       {/* Image picker */}
       {image ? (
         <Image
-        source={{
-          uri: image,
-        }}
-        className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
-      />
+          source={{
+            uri: image,
+          }}
+          className="w-52 aspect-[3/4] rounded-lg bg-slate-300"
+        />
       ) : (
         <View className="w-52 aspect-[3/4] rounded-lg bg-slate-300" />
       )}
-      
 
       <Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
         Change
